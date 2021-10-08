c54e845f860825d3a40ed49315d498542748f331
kimberly lin <kimberly.lin10@gmail.com>
2021-10-08 10:21:46 -0700

    make server and config file changes

--- config.js
+++ config.js
@@ -5,15 +5,15 @@
 require("dotenv").config();
 require("colors");
 
-const SECRET_KEY = process.env.SECRET_KEY || "secret-dev";
+const SECRET_KEY = process.env.SECRET_KEY || 'i-have-a-secret';
 
 const PORT = +process.env.PORT || 3001;
 
 // Use dev database, testing database, or via env var, production database
 function getDatabaseUri() {
   return (process.env.NODE_ENV === "test")
-      ? "jobly_test"
-      : process.env.DATABASE_URL || "jobly";
+    ? "jobly_test"
+    : process.env.DATABASE_URL || "jobly";
 }
 
 // Speed up bcrypt during tests, since the algorithm safety isn't being tested
